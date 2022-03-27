fdd7ff0c6ab676cefb90491cd0b14daf0b028de4
Josh Salsbury <joshuajsalsbury@gmail.com>
2022-03-26 21:34:44 -0500

    GalleryItem.jsx; add: toggleShowHideFunction and ternary to toggle

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -3,28 +3,44 @@ import { useState, useEffect } from 'react';
 function GalleryItem({galleryItem, addLike}) {
     // console.log('clicked!')
 
-    let [likes, setLikes] = useState(false);
+    // const [likes, setLikes] = useState(0);
+    const [isShowing, setIsShowing] = useState(false);
+ 
 
     const handleLikes = () => {
         console.log('Added like!');
         addLike(galleryItem);
     }
 
-    const handleToggle = () => {
+    const toggleShowHide = () => {
         console.log('Checked Description!');
+        // if (galleryItem.description === true) {
+        //     return 
+        // }
+        setIsShowing(!isShowing);
+        
+        // if(isShowing === false) {
+        //     console.log('false');
+        // } else if (isShowing === true) {
+        //     console.log('true');
+        // }
+        };
+       
+        console.log(isShowing);
 
-    }
+   
 
     return(
         <>
-        <div id="photoId" onClick={handleToggle}>
-        <img key="galleryItem.id" className="photo" src={galleryItem.path}/>
+        <div id="photoId" onClick={toggleShowHide}>
+        {isShowing ? <p>{galleryItem.description}</p> : <img key="galleryItem.id" className="photo" src={galleryItem.path}/>}
         </div>
         <div id="btnId">
         <button onClick={handleLikes}>Click To Like!</button>
         </div>
         <div id="likesCounterId">
         <>{galleryItem.likes} People Liked Your Photo</>
+        {/* {setIsShowing()} */}
         </div>
         </>
     )
